427358d2170c6fa9047d7eff3e6e6527102aa53c
Caroline Protat <caroline.protat@hotmail.fr>
2023-12-08 16:37:10 +0100

    réinsertion NavBar dans la page App

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/Navbar/Navbar'
 import Routing from './components/Routing/Routing';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <header>
-        <Navbar/>
-      </header>
-      <main>
         <Routing />
-      </main>
   </React.StrictMode>
 );
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,15 @@
+import Navbar from "../../components/Navbar/Navbar";
 import Home from "../../components/Home/Home";
 import Presentation from "../../components/Presentation/Presentation";
 import Portfolio from "../../components/Portfolio/Portfolio";
 import Contact from "../../components/Contact/Contact";
-import './App.scss';
 
 function App() {
   return(
     <>
+      <header>
+        <Navbar/>
+      </header>
       <main>
         <Home/>
         <Presentation/>
