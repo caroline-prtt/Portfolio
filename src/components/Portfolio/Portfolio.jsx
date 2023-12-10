f8d1479ec44ecf63bdbab700c0992bae915d8836
Caroline Protat <caroline.protat@hotmail.fr>
2023-12-10 21:42:38 +0100

    ajustement du composant Portfolio avec basculement du lien pour la description des projets  vers le composant Card

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,28 +1,22 @@
-import { Link } from "react-router-dom";
 import Card from "../Card/Card";
 import data from "../../data.json";
 import "./portfolio.scss"
 
 function Portfolio() {
 
-    const scrollToTop = () => {
-        window.scrollTo(0,0);
-    }
-
     return(
         <div id="portfolio" className="portfolio">
             <h2 className="portfolio__title">PORTFOLIO</h2>
             <div className="portfolio__cards">
                 {data.map((item, index) => (
-                    <Link key={item.id} to={`/project/${item.id}`} onClick={scrollToTop} className="portfolio__cards__link">
-                        <Card 
-                            key={index}
-                            cover={item.cover} 
-                            title={item.title} 
-                            subtitle={item.subtitle}
-                            keywords={item.keywords}
-                        />
-                    </Link> 
+                    <Card 
+                        key={index}
+                        id={item.id}
+                        cover={item.cover} 
+                        title={item.title} 
+                        subtitle={item.subtitle}
+                        keywords={item.keywords}
+                    />
                 ))}
             </div>
         </div>
