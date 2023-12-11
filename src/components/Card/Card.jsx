06e763ce81fa5eab0f57feb4556a73772b13799d
Caroline Protat <caroline.protat@hotmail.fr>
2023-12-11 01:01:29 +0100

    réalisation du composant Card et son design

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,35 @@
+import { Link } from "react-router-dom";
 import './card.scss';
 
 function Card(props) {
-    return(
-        <div className='card'>
-            <div className='card__image'>
-                <img src={props.cover} alt="" />
-            </div>
-            <div className='card__content'>
-                <h4 className='card__content__title'>{props.title}</h4>
 
+    const scrollToTop = () => {
+        window.scrollTo(0,0);
+    }
+
+    return(
+        <div className='container'>
+
+            <div className="card">
+
+                <div className='card__image'>
+                    <img src={props.cover} alt="Page d'accueil du site web"/>
+                </div>
+
+                <div className="card__keywords">
+                    {props.keywords.map((keyword, index) =>(
+                        <div key={index} className="keyword">{keyword}</div>
+                    ))}
+                </div>
+                        
+                <div className='card__description'>
+                    <h4 className='card__description__title'>{props.title}</h4>
+                    <h5 className='card__description__subtitle'>{props.subtitle}</h5>
+                </div>
             </div>
+
+            <Link to={`/project/${props.id}`} onClick={scrollToTop} className="link">EN SAVOIR PLUS</Link>
+
         </div>
     )
 }
