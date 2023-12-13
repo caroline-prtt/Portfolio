576174bffafcf7c38b25fcffba9d04405b86b598
Caroline Protat <caroline.protat@hotmail.fr>
2023-12-13 18:34:57 +0100

    intégration de la page d'erreur 404

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
+import "./errorPage.scss"
+
 function ErrorPage (){
     return (
-        <p>Page d'erreur 404</p>
+       <section className="error">
+            <div className="error__404">
+                <h2 className="error__404 dark-green">404</h2>
+                <h2 className="error__404 light-green">404</h2>
+                <h2 className="error__404 light-yellow">404</h2>
+            </div>
+            <p>La page que vous demandez n'existe pas.</p>
+            <Link to="/#home" className="error__returnLink">Retourner sur la page d'accueil</Link>
+       </section>
     )
 }
 
