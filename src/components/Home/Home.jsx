f28c25207d9011070083daf34f31ba59c46bea04
Caroline Protat <caroline.protat@hotmail.fr>
2023-12-14 12:11:58 +0100

    Modification des liens pour des liens Link de react router et ajout de l'icone enveloppe pour renvoyer au formulaire de contact

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,8 @@
+import { Link } from 'react-router-dom';
+import { HashLink } from "react-router-hash-link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import photo from '../../assets/portrait/photo.png'
 import "./home.scss"
 
@@ -13,8 +16,9 @@ function Home() {
         <p className="home__title subtitle">Basée sur Lyon, je suis à la recherche de ma première expérience en tant que développeuse front-end junior.</p>
         <p className="home__title subtitle">Où me trouver :</p>
         <div className="home__title icons">
-          <a href="https://www.linkedin.com/in/caroline-protat-b67761231/" title="Mon profil Linkedin"><FontAwesomeIcon icon={faLinkedin} /></a>
-          <a href="https://github.com/caroline-prtt" title="Mon compte GitHub"><FontAwesomeIcon icon={faGithub} /></a>
+          <Link to="https://www.linkedin.com/in/caroline-protat-b67761231/" title='Mon profil Linkedin' className='home__title icons__link'><FontAwesomeIcon icon={faLinkedin} /></Link>
+          <Link to="https://github.com/caroline-prtt" title="Mon compte GitHub" className="home__title icons__link"><FontAwesomeIcon icon={faGithub} /></Link>
+          <HashLink smooth to="/#contact" title="Par mail" className="home__title icons__link"><FontAwesomeIcon icon={faEnvelope} /></HashLink>
         </div>
       </div>
       <div className="home__picture">
